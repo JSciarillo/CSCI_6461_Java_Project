f4276684403b79d98778d401f2d1840d1e927e53
Oliver Krisetya <okrisetya@gmail.com>
2026-02-24 08:42:16 -0500

    Simple fetch setup and test

--- src/CPU.java
+++ src/CPU.java
@@ -1,16 +1,48 @@
 package src;
 
-public class CPU {
-    int[] R = new int[4]; // R0 - R3
-    int pc;
-    int mar;
-    int mbr;
-    int ir;
+public final class CPU {
+    private final int[] R = new int[4]; // R0 - R3
 
-    public void signleStep(){}
+    private int pc;
+    private int mar;
+    private int mbr;
+    private int ir;
 
-    private void fetch(){}
+    private boolean halted;
+
+    public void reset() {
+        for (int i = 0; i < R.length; i++) { R[i] = 0; }
+        pc = mar = mbr = ir = 0;
+        halted = false;
+    }
+
+    public void signleStep(Memory mem){
+        if (halted) return;
+
+        fetch(mem);
+    }
+
+    private void fetch(Memory mem){
+        mar = pc;
+        mbr = mem.read(mar);
+        ir = mbr;
+        pc = pc + 1;
+    }
+    
     private void decode(){}
     private void computeEffectiveAddress(){}
     private void execute(){}
+
+    public int getPC()  { return pc; }
+    public int getMAR() { return mar; }
+    public int getMBR() { return mbr; }
+    public int getIR()  { return ir; }
+
+    public int getR(int idx) { return R[idx]; }
+    public void setR(int idx, int value) { R[idx] = value; }
+
+    public void setPC(int pc) { this.pc = pc; }
+
+    public boolean isHalted() { return halted; }
+    public void setHalted(boolean halted) { this.halted = halted; }
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -1,14 +1,62 @@
 package src;
 
-public class Simulator {
-    CPU cpu;
-    Memory mem;
+public final class Simulator {
+    private final CPU cpu;
+    private final Memory mem;
 
-    public void reset(){}
-    public void ipl(){}
-    public void singleStep(){}
-    public void run(){}
-    public void depositMemory(){}
-    public void setRegister(){}
-    public void setPC(){}
+    public Simulator(int memSizeWords, int wordBits) {
+        this.cpu = new CPU();
+        this.mem = new Memory(memSizeWords, wordBits);
+        this.cpu.reset();
+    }
+
+    public void reset(){
+        cpu.reset();
+        // Maybe add clear memory
+    }
+
+    public void depositMemory(int address, int value) {
+        mem.write(address, value);
+    }
+
+    public int readMemory(int address) {
+        return mem.read(address);
+    }
+
+    public void setPC(int address) {
+        cpu.setPC(address);
+    }
+
+    public void setRegister(int rIndex, int value) {
+        cpu.setR(rIndex, value);
+    }
+
+    public void singleStep(){
+        cpu.signleStep(mem);
+    }
+
+    public CPU getCPU() { return cpu; }
+    public Memory getMemory() { return mem; }
+
+    public int getMemoryAtMAR() {
+        return mem.read(cpu.getMAR());
+    }
+
+    // Temp test
+    public static void main(String[] args) {
+        Simulator s = new Simulator(2048, 16);
+
+        int A = 100;
+        int X = 0x1234;
+
+        s.depositMemory(A, X);
+        s.setPC(A);
+
+        s.singleStep();
+
+        System.out.println("MAR=" + s.getCPU().getMAR());
+        System.out.println("IR=" + s.getCPU().getIR());
+        System.out.println("PC=" + s.getCPU().getPC());
+        System.out.println("MEM[MAR]=" + s.getMemoryAtMAR());
+    }
 }
